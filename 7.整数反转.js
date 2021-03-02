4055992117cbe082c9c0c69799b6c30d3e9cfe33
LuSuguru <429150413@qq.com>
2021-03-02 09:44:57 +0800

    feat(3.2):

---
+++ 7.整数反转.js
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=7 lang=javascript
+ *
+ * [7] 整数反转
+ */
+
+// @lc code=start
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function (x) {
+  const str = x + ''
+  const strArr = str.match(/\d/g)
+
+  let newNum = 0
+  for (let i = 0; i < strArr.length; i++) {
+    newNum = newNum + 10 ** i * strArr[i]
+  }
+
+  if (x < 0) {
+    newNum = -newNum
+  }
+
+  if (newNum < -(2 ** 31) || newNum > 2 ** 31 - 1) {
+    return 0
+  }
+
+  return newNum
+};
+// @lc code=end
+
