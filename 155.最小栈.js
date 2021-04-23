2f769a08fc9cb3fdf9172e12cf94f5af259890a9
LuSuguru <429150413@qq.com>
2021-04-23 10:52:33 +0800

    feat(3 OK):

---
+++ 155.最小栈.js
@@ -0,0 +1,73 @@
+/*
+ * @Date: 2021-04-14 18:23:28
+ * @Author: 芦杰
+ * @Description: Do not edit
+ * @LastEditors: 芦杰
+ * @LastEditTime: 2021-04-14 18:51:02
+ */
+/*
+ * @lc app=leetcode.cn id=155 lang=javascript
+ *
+ * [155] 最小栈
+ */
+
+const { CHAR_0 } = require("picomatch/constants");
+
+// @lc code=start
+/**
+ * initialize your data structure here.
+ */
+var MinStack = function () {
+  this.stack = []
+  this.minStack = []
+};
+
+/** 
+ * @param {number} val
+ * @return {void}
+ */
+MinStack.prototype.push = function (val) {
+  this.stack.push(val)
+  const min = this.minStack[0]
+  console.log(val, min)
+  if (min === undefined || val <= min) {
+    this.minStack.unshift(val)
+  }
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function () {
+  const min = this.minStack[0]
+  const current = this.stack.pop()
+  if (min === current) {
+    this.minStack.shift()
+  }
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function () {
+  const { stack } = this
+  return stack[stack.length - 1]
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function () {
+  return this.minStack[0]
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(val)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
+// @lc code=end
+
--- 3.无重复字符的最长子串.js
+++ 3.无重复字符的最长子串.js
@@ -1,4 +1,11 @@
 /*
+ * @Date: 2021-03-02 09:22:13
+ * @Author: 芦杰
+ * @Description: Do not edit
+ * @LastEditors: 芦杰
+ * @LastEditTime: 2021-04-23 10:39:52
+ */
+/*
  * @lc app=leetcode.cn id=3 lang=javascript
  *
  * [3] 无重复字符的最长子串
@@ -6,11 +13,30 @@
 
 // @lc code=start
 /**
+ * 
  * @param {string} s
  * @return {number}
  */
-var lengthOfLongestSubstring = function(s) {
+var lengthOfLongestSubstring = function (s) {
+  let map = {} // 存放子串每个字符的位置
 
+  // two-pointers 和 sliding-window
+  let start = -1 // 开始位置
+  let maxLength = 0
+
+  for (let i = 0; i < s.length; i++) {
+    const arrCurrent = s[i]
+
+    if (arrCurrent in map) {
+      start = Math.max(map[arrCurrent], start)
+    }
+
+    map[arrCurrent] = i
+    maxLength = Math.max(i - start, maxLength)
+  }
+
+  return maxLength
 };
 // @lc code=end
+
 
