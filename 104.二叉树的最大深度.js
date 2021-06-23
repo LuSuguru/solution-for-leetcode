e4dff99916c080346f53f7e522b21ad7b3caff68
LuSuguru <429150413@qq.com>
2021-06-23 10:14:17 +0800

    feat: 104

--- 104.二叉树的最大深度.js
+++ 104.二叉树的最大深度.js
@@ -17,9 +17,38 @@
  * @param {TreeNode} root
  * @return {number}
  */
-const maxDepth = (root, depth = 0) => (
-  root
-    ? Math.max(maxDepth(root.left, depth + 1), maxDepth(root.right, depth + 1))
-    : depth)
+// 深度优先遍历
+function maxDepth(root, depth = 0) {
+  if (root) {
+    return Math.max(maxDepth(root.left, depth + 1), maxDepth(root.right, depth + 1))
+  } else {
+    return depth
+  }
+}
 
+// 广度优先遍历
+function maxDepth(root) {
+  if (!root) return 0
+  const queue = [root]
+
+  let depth = 0
+  while (queue.length) {
+    let count = queue.length
+    depth++
+
+    while (count) {
+      const node = queue.shift()
+
+      if (node?.left) {
+        queue.push(node.left)
+      }
+      if (node?.right) {
+        queue.push(node.right)
+      }
+
+      count--
+    }
+  }
+  return depth
+}
 // @lc code=end
