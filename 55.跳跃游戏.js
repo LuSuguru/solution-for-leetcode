4c20860d63152df73f1729f39475c3ecaa599688
LuSuguru <429150413@qq.com>
2021-07-14 09:49:51 +0800

    feat: 55

---
+++ 55.跳跃游戏.js
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=55 lang=javascript
+ *
+ * [55] 跳跃游戏
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+function canJump(nums) {
+  let max = 0
+
+  for (let i = 0; i < nums.length; i++) {
+    if (i > max) {
+      return false
+    }
+    max = Math.max(max, i + nums[i])
+  }
+  return true
+}
+// @lc code=end
