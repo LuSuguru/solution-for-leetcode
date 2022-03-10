2f7a61826490b685d51034743dcc4d6c5a22e29b
LuSuguru <429150413@qq.com>
2022-03-10 10:28:28 +0800

    feat: 287

--- 287.寻找重复数.js
+++ 287.寻找重复数.js
@@ -9,7 +9,24 @@
  * @param {number[]} nums
  * @return {number}
  */
-const findDuplicate = function (nums) {
+function findDuplicate(nums) {
+  // 由于数组里的值是不会大于nums.length，可以当做一个链表来做，重复数构成的是一个环，要找的是环的起始点
+  let fast = 0
+  let slow = 0
 
+  while (true) {
+    fast = nums[nums[fast]]
+    slow = nums[slow]
+
+    if (fast === slow) {
+      fast = 0
+      while (nums[fast] !== nums[slow]) {
+        fast = nums[fast]
+        slow = nums[slow]
+      }
+
+      return nums[slow]
+    }
+  }
 }
 // @lc code=end
