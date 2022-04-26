b966e46c827d415a67be81699efecd1fdd2ac130
LuSuguru <429150413@qq.com>
2022-04-26 09:35:01 +0800

    feat: 剑指offer 46

--- 剑指 Offer 46. 把数字翻译成字符串.js
+++ 剑指 Offer 46. 把数字翻译成字符串.js
@@ -3,5 +3,19 @@
  * @return {number}
  */
 function translateNum(num) {
+  const numStr = num + ''
+  const dp = new Array(numStr.length + 1).fill(1)
 
+  for (let i = 1; i < numStr.length; i++) {
+    const cur = numStr[i]
+    const pre = numStr[i - 1]
+
+    if (pre == 1 || (pre == 2 && cur >= 0 && cur <= 5)) {
+      dp[i + 1] = dp[i] + dp[i - 1]
+    } else {
+      dp[i + 1] = dp[i]
+    }
+  }
+
+  return dp[dp.length - 1]
 }
