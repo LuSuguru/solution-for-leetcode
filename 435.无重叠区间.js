c41c0f01816d111545fc832cbd51285232cec233
LuSuguru <429150413@qq.com>
2022-07-21 10:08:40 +0800

    feat: 435

--- 435.无重叠区间.js
+++ 435.无重叠区间.js
@@ -9,8 +9,19 @@
  * @param {number[][]} intervals
  * @return {number}
  */
-var eraseOverlapIntervals = function(intervals) {
+function eraseOverlapIntervals(intervals) {
+  // 右区间排序
+  intervals.sort((a, b) => a[1] - b[1])
 
-};
+  let res = 1
+  let end = intervals[0][1]
+  for (let i = 1; i < intervals.length; i++) {
+    if (end <= intervals[i][0]) {
+      end = intervals[i][1]
+      res++
+    }
+  }
+
+  return intervals.length - res
+}
 // @lc code=end
-
