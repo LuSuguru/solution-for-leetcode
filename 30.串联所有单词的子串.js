5879b2a40dc6c18a619c39498f5675b665b09de5
LuSuguru <429150413@qq.com>
2022-09-14 10:11:12 +0800

    feat: 30

--- 30.串联所有单词的子串.js
+++ 30.串联所有单词的子串.js
@@ -10,6 +10,7 @@
  * @param {string[]} words
  * @return {number[]}
  */
+// 我的解法
 function findSubstring(s, words) {
   const wordLength = words[0].length
   const num = words.length
@@ -66,5 +67,55 @@ function findSubstring(s, words) {
   }
 
   return result
+}
+
+// 官方，滑动窗口
+function findSubstring(s, words) {
+  const res = []
+  const m = words.length;
+  const n = words[0].length;
+  const ls = s.length
+
+  for (let i = 0; i < n; i++) {
+    if (i + m * n > ls) {
+      break
+    }
+
+    const differ = new Map()
+
+    for (let j = 0; j < m; j++) {
+      const word = s.substring(i + j * n, i + (j + 1) * n)
+      differ.set(word, (differ.get(word) || 0) + 1)
+    }
+
+    for (const word of words) {
+      differ.set(word, (differ.get(word) || 0) - 1)
+      if (differ.get(word) === 0) {
+        differ.delete(word)
+      }
+    }
+
+    for (let start = i; start < ls - m * n + 1; start += n) {
+      if (start !== i) {
+        let word = s.substring(start + (m - 1) * n, start + m * n)
+        differ.set(word, (differ.get(word) || 0) + 1)
+
+        if (differ.get(word) === 0) {
+          differ.delete(word)
+        }
+
+        word = s.substring(start - n, start)
+        differ.set(word, (differ.get(word) || 0) - 1)
+        if (differ.get(word) === 0) {
+          differ.delete(word)
+        }
+      }
+
+      if (differ.size === 0) {
+        res.push(start)
+      }
+    }
+  }
+  return res
 }
 // @lc code=end
